12831e385b099b4d4dc44b5c4bd9a6a8c4eb408c
divyashantkumar <divyashantkumar000@gmail.com>
2025-06-18 16:04:55 +0530

    Added: HTML/CSS assignment files

---
+++ forms.js
@@ -0,0 +1,9 @@
+// const inpEle = document.getElementById("username");
+// // const emailEle = document.getElementById("email");
+
+// console.log(inpEle.value);
+
+// function printValue() {
+//     console.log(inpEle.value);
+//     // console.log(emailEle.value);
+// }
